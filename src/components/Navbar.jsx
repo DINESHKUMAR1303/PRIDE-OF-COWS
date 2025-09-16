5eb34855fa0c004f7416d2941ac20113c5f48e96
DINESHKUMAR1303 <dineshkumar41083@gmail.com>
2025-09-16 12:05:29 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,40 @@
 import React from "react";
+import logo from "../images/logo.png";        // Pride of Cows logo
+import loginIcon from "../images/user.svg";  // your custom login icon
+import cartIcon from "../images/cart.svg";    // your custom cart icon
+import { FaMapMarkerAlt } from "react-icons/fa"; // keep only map pin
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="left">
-        <img src="/logo.png" alt="logo" className="logo" />
-        <button className="pincode">Enter a Pincode</button>
+      {/* Left Section */}
+      <div className="navbar-left">
+        <img src={logo} alt="Pride of Cows" className="logo" />
+        <button className="pincode-btn">
+          <FaMapMarkerAlt className="icon" />
+          ENTER A PINCODE
+        </button>
       </div>
 
+      {/* Center Menu */}
       <ul className="menu">
-        <li>Shop</li>
-        <li>Learn</li>
-        <li>Blog</li>
+        <li>Shop ▾</li>
+        <li>Learn ▾</li>
+        <li>Blog ▾</li>
         <li>Gift card</li>
       </ul>
 
-      <div className="right">
-        <button>Login</button>
-        <button>Cart (0)</button>
+      {/* Right Section */}
+      <div className="navbar-right">
+        <div className="login">
+          <img src={loginIcon} alt="login" className="right-icon" />
+          <span className="login-text">LOGIN</span>
+        </div>
+        <div className="cart">
+          <img src={cartIcon} alt="cart" className="right-icon" />
+          <span className="cart-text">CART</span>
+          <span className="cart-count">0</span>
+        </div>
       </div>
     </nav>
   );
