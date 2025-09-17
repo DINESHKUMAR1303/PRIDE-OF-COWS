e2cde16c937c107de480ef0f70f72275f697a096
DINESHKUMAR1303 <dineshkumar41083@gmail.com>
2025-09-17 12:18:41 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,24 @@ import React, { useState } from "react";
 import logo from "../images/icons/logo.png";
 import loginIcon from "../images/icons/user.svg";
 import cartIcon from "../images/icons/cart.svg";
+
+// ✅ Section Icons
+import enquiryIcon from "../images/icons/enquiry.svg";
+import helpIcon from "../images/icons/help.svg";
+import contactIcon from "../images/icons/contact.svg";
+import faqIcon from "../images/icons/faq.svg";
+import aboutIcon from "../images/icons/about.svg";
+import lifestyleIcon from "../images/icons/lifestyle.svg";
+import recipesIcon from "../images/icons/recipies.svg";
+
+// ✅ Footer Icons
+import appstoreIcon from "../images/icons/appstore.svg";
+import playstoreIcon from "../images/icons/playstore.svg";
+import instagramIcon from "../images/icons/instagram.svg";
+import facebookIcon from "../images/icons/facebook.svg";
+import twitterIcon from "../images/icons/twitter.svg";
+import youtubeIcon from "../images/icons/youtube.svg";
+
 import { FaMapMarkerAlt, FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
@@ -9,6 +27,7 @@ const Navbar = () => {
 
   return (
     <>
+      {/* ================== NAVBAR ================== */}
       <nav className="navbar">
         {/* Left Section */}
         <div className="navbar-left">
@@ -50,7 +69,7 @@ const Navbar = () => {
         </div>
       </nav>
 
-      {/* Sidebar / Mobile Menu */}
+      {/* ================== SIDEBAR / MOBILE MENU ================== */}
       <div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
         <div className="mobile-header">
           <div className="login">
@@ -62,33 +81,52 @@ const Navbar = () => {
           </button>
         </div>
 
+        {/* Menu Links with Icons */}
         <ul className="mobile-links">
-          <li>Shop ▾</li>
-          <li>Support</li>
-          <li>Enquiry</li>
-          <li>Help</li>
-          <li>Contact</li>
-          <li>FAQ</li>
-          <li>Learn More</li>
-          <li>About Us</li>
-          <li>Life Style</li>
-          <li>Recipes</li>
+          <li>
+            <span>Shop ▾</span>
+          </li>
+
+          <li className="section-title">Support</li>
+          <li>
+            <img src={enquiryIcon} alt="enquiry" className="right-icon" /> Enquiry
+          </li>
+          <li>
+            <img src={helpIcon} alt="help" className="right-icon" /> Help
+          </li>
+          <li>
+            <img src={contactIcon} alt="contact" className="right-icon" /> Contact
+          </li>
+          <li>
+            <img src={faqIcon} alt="faq" className="right-icon" /> FAQ
+          </li>
+
+          <li className="section-title">Learn More</li>
+          <li>
+            <img src={aboutIcon} alt="about" className="right-icon" /> About Us
+          </li>
+          <li>
+            <img src={lifestyleIcon} alt="lifestyle" className="right-icon" /> Life Style
+          </li>
+          <li>
+            <img src={recipesIcon} alt="recipes" className="right-icon" /> Recipes
+          </li>
         </ul>
 
-        {/* Footer inside sidebar */}
+        {/* ================== FOOTER ================== */}
         <div className="mobile-footer">
           <p>Get The App</p>
           <div className="app-icons">
-            <i className="fab fa-apple"></i>
-            <i className="fab fa-google-play"></i>
+            <img src={appstoreIcon} alt="App Store" />
+            <img src={playstoreIcon} alt="Google Play" />
           </div>
 
           <p>Follow Us</p>
           <div className="social-icons">
-            <i className="fab fa-instagram"></i>
-            <i className="fab fa-facebook"></i>
-            <i className="fab fa-twitter"></i>
-            <i className="fab fa-youtube"></i>
+            <img src={instagramIcon} alt="Instagram" />
+            <img src={facebookIcon} alt="Facebook" />
+            <img src={twitterIcon} alt="Twitter" />
+            <img src={youtubeIcon} alt="YouTube" />
           </div>
         </div>
       </div>
